0b59af736bfbacd595d54d01416146d3ac808afe
Vival29 <virginie.f.r.valentin@gmail.com>
2022-11-22 14:27:01 +0100

    Added details in Console

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -31,7 +31,7 @@ public class Console {
         System.out.println("__________________________________________________________________________________________");
         System.out.println("Bonne partie de Lotto!!                           Vous jouez la partie no: " + compteur);
         while (running) {
-            //System.out.println("Entrer le numero tire, pressez [ENTER] ou choisissez v1, v2, v3 ou del ou exit");
+            System.out.println("Entrer le numero tire, pressez [ENTER] ou choisissez un mot clé");
             Scanner command = new Scanner(System.in);
             String entry = command.nextLine();
             if(isNumeric(entry)){
